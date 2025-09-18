e9ec35167d33ed6ce34d7261559c071af5958084
typicalparsa <typicalparsa@gmail.com>
2025-09-18 18:14:42 +0330

    sll append_node

--- data_structure/linked_list/singly_linked_list/sll.cpp
+++ data_structure/linked_list/singly_linked_list/sll.cpp
@@ -48,4 +48,16 @@ class SLL{
                 cout << "null " << endl;
             }
         }
+
+        void append_node(int value){
+            Node* new_node = new Node(value);
+            if (this->length == 0){
+                this->head = new_node;
+                this->tail = new_node;
+            }else{
+                this->tail->next = new_node;
+                this->tail = new_node;
+            }
+            this->length++;
+        }
 };
