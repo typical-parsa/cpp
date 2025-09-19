ff26d03c1e0e6832195a40449b803bc687fbfcc3
typicalparsa <typicalparsa@gmail.com>
2025-09-19 18:10:11 +0330

    sll prepend_node

--- data_structure/linked_list/singly_linked_list/sll.cpp
+++ data_structure/linked_list/singly_linked_list/sll.cpp
@@ -82,4 +82,16 @@ class SLL{
             delete temp_node;
             this->length--;
         }
+
+        void prepend_node(int value){
+            Node* new_node = new Node(value);
+            if (this->length == 0){
+                this->head = new_node;
+                this->tail = new_node;
+            }else{
+                new_node->next = this->head;
+                this->head = new_node;
+            }
+            this->length++;
+        }
 };
