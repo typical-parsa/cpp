c99be6494400cbc5ca9740a1fd28e2df13ee244e
typicalparsa <typicalparsa@gmail.com>
2025-09-19 18:25:58 +0330

    sll set_node_value

--- data_structure/linked_list/singly_linked_list/sll.cpp
+++ data_structure/linked_list/singly_linked_list/sll.cpp
@@ -127,4 +127,19 @@ class SLL{
             }
         }
 
+        void set_node_value(int index, int value){
+            if (this->length == 0){
+                cout << "List is empty!" << endl;
+                return;
+            }else if (index < 0 || index >= this->length){
+                cout << "Invalid index!" << endl;
+                return; 
+            }else{
+                Node* temp_node = this->get_node_by_index(index);
+                if (temp_node != nullptr){
+                    temp_node->value = value;
+                }
+            }
+        }
+
 };
