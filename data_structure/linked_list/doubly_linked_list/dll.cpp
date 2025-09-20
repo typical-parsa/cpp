882a4de4023f923d61c0e582558250cc8cd92eb7
typicalparsa <typicalparsa@gmail.com>
2025-09-20 03:40:24 +0330

    dll DLL class

--- data_structure/linked_list/doubly_linked_list/dll.cpp
+++ data_structure/linked_list/doubly_linked_list/dll.cpp
@@ -6,9 +6,33 @@ class Node{
     public:
         int value;
         Node* next;
+        Node* previous;
 
         Node(int value){
             this->value = value;
             this->next = nullptr;
+            this->previous = nullptr;
+        }
+};
+
+class DLL{
+    private:
+        Node* head;
+        Node* tail;
+        int lenght;
+
+    public:
+        DLL(){
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->lenght = 0;
+        }
+
+        ~DLL(){
+           while (this->head != nullptr){
+                Node* temp = this->head;
+                this->head = this->head->next;
+                delete temp;
+            }
         }
 };
