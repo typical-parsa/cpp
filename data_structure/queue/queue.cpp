e349173663b30538ebb30e66085c65c7f34a1062
typicalparsa <typicalparsa@gmail.com>
2025-09-29 17:23:17 +0330

    queue enqueue_node

--- data_structure/queue/queue.cpp
+++ data_structure/queue/queue.cpp
@@ -6,6 +6,11 @@ class Node{
     public:
         int value;
         Node* next;
+
+        Node(int value){
+            this->value = value;
+            this->next = nullptr;
+        }
 };
 
 class QUEUE{
@@ -19,5 +24,17 @@ class QUEUE{
             this->front = nullptr;
             this->rear = nullptr;
             this->length = 0;
+        }
+
+        void enqueue_node(int value){
+            Node* new_node = new Node(value);
+            if (this->length == 0){
+                this->front = new_node;
+                this->rear = new_node;
+            }else{
+                this->rear->next = new_node;
+               this->rear = new_node;
+            }
+            this->length++;
         }
 };
