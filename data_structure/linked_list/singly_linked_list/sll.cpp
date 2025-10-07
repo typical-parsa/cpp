28750915e4c908cc45de7cfd5e55800f6dfdf44b
typicalparsa <typicalparsa@gmail.com>
2025-10-07 15:55:50 +0330

    cpp sll print_list

--- data_structure/linked_list/singly_linked_list/sll.cpp
+++ data_structure/linked_list/singly_linked_list/sll.cpp
@@ -25,4 +25,19 @@ class SLL{
             this->tail = nullptr;
             this->length = 0;
         }
+
+        void print_list(){
+            if (this->length == 0){
+                cout << "List is empty!" << endl;
+                return;
+            }else{
+                Node* temp_node = this->head;
+                cout << "List Items: " << endl;
+                while(temp_node != nullptr){
+                    cout << temp_node->value << " -> ";
+                    temp_node = temp_node->next;
+                }
+                cout << " null" << endl;
+            }
+        }
 };
