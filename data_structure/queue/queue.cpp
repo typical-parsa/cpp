b3dfd5bca2dccbc3ff2aa03b0f44302c0bc6baab
typicalparsa <typicalparsa@gmail.com>
2025-10-15 11:39:02 +0330

    cpp queue QUEUE class

--- data_structure/queue/queue.cpp
+++ data_structure/queue/queue.cpp
@@ -13,3 +13,25 @@ class Node{
             this->next = nullptr;
         }
 };
+
+class QUEUE{
+    private:
+        Node* rear;
+        Node* front;
+        int length;
+    
+    public:
+        QUEUE(){
+            this->rear = nullptr;
+            this->front = nullptr;
+            this->length = 0;
+        }
+
+        ~QUEUE(){
+            while(this->front != nullptr){
+                Node* temp_node = this->rear;
+                this->rear = this->rear->next;
+                delete temp_node;
+            }
+        }
+};
