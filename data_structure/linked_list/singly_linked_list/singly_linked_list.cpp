718dfeb2684738a23d23814ee0fb0adee25bec08
typicalparsa <typicalparsa@gmail.com>
2025-10-25 12:27:05 +0330

    cpp singly_linked_list delete_first_node

--- data_structure/linked_list/singly_linked_list/singly_linked_list.cpp
+++ data_structure/linked_list/singly_linked_list/singly_linked_list.cpp
@@ -96,4 +96,20 @@ class SLL{
             this->length++;
             return true;
         }
+
+        bool delete_first_node(){
+            if (this->length == 0){
+                cout << "List is empty!" << endl;
+                return false;
+            }
+            Node* temp_node = this->head;
+            if (this->length == 1){
+                this->head = nullptr;
+                this->tail = nullptr;
+            }else{
+                this->head = this->head->next;
+            }
+            this->length--;
+            return true;
+        }
 };
