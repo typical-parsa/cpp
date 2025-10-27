5f226fddd0487bbc644f29bb47bebb3cb536b33d
typicalparsa <typicalparsa@gmail.com>
2025-10-27 15:25:14 +0330

    cpp stack pop_node

--- data_structure/stack/stack.cpp
+++ data_structure/stack/stack.cpp
@@ -33,13 +33,6 @@ class Stack{
             }
         }
 
-        bool push_node(int value){
-            Node* new_node = new Node(value);
-            new_node->next = this->top;
-            this->top = new_node;
-            this->height++;
-        }
-
         void print_stack(){
             if (this->height == 0){
                 cout << "Stack is empty!" << endl;
@@ -50,5 +43,29 @@ class Stack{
                     cout << temp_node->value << endl;
                 }
             }
+        }
+
+        bool push_node(int value){
+            Node* new_node = new Node(value);
+            new_node->next = this->top;
+            this->top = new_node;
+            this->height++;
+        }
+
+        int pop_node(){
+            if (this->height == 0){
+                cout << "Stack is empty!" << endl;
+                return INT_MIN;
+            }
+            Node* temp_node = this->top;
+            int popped_value = temp_node->value;
+            if (this->height == 0){
+                this->top = nullptr;
+            }else{
+                this->top = this->top->next;
+            }
+            this->height--;
+            delete temp_node;
+            return popped_value;
         }
 };
