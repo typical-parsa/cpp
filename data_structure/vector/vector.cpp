777ffcdf71c86b3596389caaafb08a9991b829fc
typicalparsa <typicalparsa@gmail.com>
2025-10-28 14:33:53 +0330

    cpp vector resize_array

--- data_structure/vector/vector.cpp
+++ data_structure/vector/vector.cpp
@@ -9,6 +9,22 @@ class Vector{
         int length;
         int capacity;
 
+        void resize_array(){
+            int new_capacity;
+            if (this->capacity == 0){
+                new_capacity = 1;
+            }else{
+                new_capacity = this->capacity * 2;
+                int* new_array_under_hood = new int[new_capacity];
+                for (int i = 0 ; i < this->length ; i++){
+                    new_array_under_hood[i] = array_under_hood[i];
+                }
+                delete[] this->array_under_hood;
+                this->array_under_hood = new_array_under_hood;
+                this->capacity = new_capacity;
+            }
+        }
+
     public:
         Vector(int capacity){
             this->array_under_hood = new int[capacity];
