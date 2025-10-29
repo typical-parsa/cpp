c2f073d06635ba16d5fb94124ef723f6f1f6775a
typicalparsa <typicalparsa@gmail.com>
2025-10-29 14:53:21 +0330

    cpp hash_table Hash_table class

--- data_structure/hash_table/hash_table.cpp
+++ data_structure/hash_table/hash_table.cpp
@@ -15,3 +15,22 @@ class Node{
             this->next = nullptr;
         }
 };
+
+class Hash_table{
+    private:
+        int size;
+        Node** data_map;
+    
+    public:
+        Hash_table(int table_size){
+            this->size = table_size;
+            this->data_map = new Node*[size];
+            for (int i = 0 ; i < size ; i++){
+                this->data_map[i] = nullptr;
+            }
+        }
+
+        ~Hash_table(){
+            delete[] this->data_map;
+        }
+};
