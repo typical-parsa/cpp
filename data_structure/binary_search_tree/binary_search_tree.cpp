d7cb3fa902548cc578f495338f19a5846291c8d6
typicalparsa <typicalparsa@gmail.com>
2025-11-01 02:59:36 +0330

    cpp BST insert_node

--- data_structure/binary_search_tree/binary_search_tree.cpp
+++ data_structure/binary_search_tree/binary_search_tree.cpp
@@ -23,4 +23,31 @@ class BST{
         BST(){
             this->root = nullptr;
         }
+
+        bool insert_node(int value){
+            Node* new_node = new Node(value);
+            if (this->root == nullptr){
+                this->root = new_node;
+                return true;
+            }
+            Node* temp_node = this->root;
+            while (true){
+                if (new_node-value == temp_node-value){
+                    return false;
+                }
+                if (new_node->value < temp_node->value){
+                    if (temp_node->left == nullptr){
+                        temp_node->left = new_node;
+                        return true;
+                    }
+                    temp_node = temp_node->left;
+                }else{
+                    if (temp_node->right == nullptr){
+                        temp_node->right = new_node;
+                        return true;
+                    }
+                    temp_node = temp_node->right;
+                }
+            }
+        }
 };
