b3cdb4146309ee065fe1606697eacaa4f16e2adb
typicalparsa <typicalparsa@gmail.com>
2025-11-21 20:07:13 +0330

    cpp vector fix

--- data_structure/vector/vector.cpp
+++ data_structure/vector/vector.cpp
@@ -12,24 +12,32 @@ class Vector{
         void resize_array(){
             int new_capacity;
             if (this->capacity == 0){
-                new_capacity = 0;
+                new_capacity = 1;
             }else{
                 new_capacity = this->capacity * 2;
-                int* new_array_under_hood = new int[new_capacity];
+            }
+            int* new_array_under_hood = new int[new_capacity];
+            if (this->array_under_hood != nullptr){
                 for (int i = 0 ; i < this->length ; i++){
                     new_array_under_hood[i] = this->array_under_hood[i];
                 }
                 delete[] this->array_under_hood;
-                this->array_under_hood = new_array_under_hood;
-                this->capacity = new_capacity;
             }
+            this->array_under_hood = new_array_under_hood;
+            this->capacity = new_capacity;
         }
 
     public:
         Vector(int capacity){
-            this->array_under_hood = new int[capacity];
-            this->length = 0;
-            this->capacity = capacity;
+            if (capacity <= 0){
+                this->array_under_hood = nullptr;
+                this->length = 0;
+                this->capacity = 0;
+            }else{
+                this->array_under_hood = new int[capacity];
+                this->length = 0;
+                this->capacity = capacity;
+            }
         }
 
         ~Vector(){
