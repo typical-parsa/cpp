8645f381f737f2452ebc52407e7cd35d573daecd
typicalparsa <typicalparsa@gmail.com>
2025-11-22 13:20:22 +0330

    cpp bubble_sort show_array

---
+++ algorithm/sorting/bubble_sort/bubble_sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void show_array(int arr[], int size);
+
+int main(){
+
+}
+
+void show_array(int arr[], int size){
+    for (int i = 0 ; i < size ; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
