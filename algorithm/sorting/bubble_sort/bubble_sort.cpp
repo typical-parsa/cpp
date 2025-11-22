5f3f4a739bdd075d91c2289dd30a90163d4b1e11
typicalparsa <typicalparsa@gmail.com>
2025-11-22 13:26:12 +0330

    cpp bubble_sort

--- algorithm/sorting/bubble_sort/bubble_sort.cpp
+++ algorithm/sorting/bubble_sort/bubble_sort.cpp
@@ -7,7 +7,13 @@ void swap_element(int arr[], int first_index, int second_index);
 void bubble_sort(int arr[], int size);
 
 int main(){
-
+    int arr[] = {4, 3, 6, 1, 9, 2, 5, 8, 7};
+    int size = (sizeof(arr) / sizeof(int));
+    cout << "Before bubble sorting: " << endl;
+    show_array(arr, size);
+    cout << "After bubble sorting: " << endl;
+    bubble_sort(arr, size);
+    show_array(arr, size);
 }
 
 void bubble_sort(int arr[], int size){
