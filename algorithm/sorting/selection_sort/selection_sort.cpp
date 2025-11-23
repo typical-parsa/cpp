7bd3e833749fe5ee77c74476709e3f85d4474973
typicalparsa <typicalparsa@gmail.com>
2025-11-23 23:11:17 +0330

    cpp selection_sort selection_sort

--- algorithm/sorting/selection_sort/selection_sort.cpp
+++ algorithm/sorting/selection_sort/selection_sort.cpp
@@ -4,9 +4,23 @@ using namespace std;
 
 void show_array(int arr[], int size);
 void swap_element(int arr[], int first_index, int second_index);
-
+void selection_sort(int arr[], int size);
 int main(){
 
+}
+
+void selection_sort(int arr[], int size){
+    for (int i = 0 ; i < size - 1 ; i++){
+        int min_index = i;
+        for (int j = i + 1 ; j < size ; j++){
+            if (arr[j] < arr[min_index]){
+                min_index = j;
+            }
+        }
+        if (min_index != i){
+            swap_element(arr, i, min_index);
+        }
+    }
 }
 
 void swap_element(int arr[], int first_index, int second_index){
