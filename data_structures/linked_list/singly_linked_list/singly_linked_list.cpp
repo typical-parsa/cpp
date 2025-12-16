8d6c1d8f01172966f7fe9e94346fa554016f078c
typicalparsa <typicalparsa@gmail.com>
2025-12-16 21:18:18 +0330

    cpp data_structure linked_list print_list

--- data_structures/linked_list/singly_linked_list/singly_linked_list.cpp
+++ data_structures/linked_list/singly_linked_list/singly_linked_list.cpp
@@ -32,4 +32,18 @@ class SLL{
                 delete temp_node;
             }
         }
+
+        void print_list(){
+            if (this->length == 0){
+                cout << "List is empty!" << endl;
+                return;
+            }else{
+                Node* temp_node = this->head;
+                while (temp_node != nullptr){
+                    cout << temp_node->value << " ";
+                    temp_node = temp_node->next;
+                }
+                cout << endl;
+            }
+        }
 };
