5a7a4adaf9e1e963ad897dfd654ce857914c5f65
typicalparsa <typicalparsa@gmail.com>
2025-12-16 21:13:09 +0330

    cpp data_structure linked_list Node class

---
+++ data_structures/linked_list/singly_linked_list/singly_linked_list.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node* next;
+
+        Node(int value){
+            this->value = value;
+            this->next = nullptr;
+        }
+};
