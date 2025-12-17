7bdb2357aaf97d5bec6fdca85b99994ebce746ac
typicalparsa <typicalparsa@gmail.com>
2025-12-17 23:39:36 +0330

    cpp data_structure doubly_linked_list DLL classg

--- data_structures/linked_list/doubly_linked_list/doubly_linked_list.cpp
+++ data_structures/linked_list/doubly_linked_list/doubly_linked_list.cpp
@@ -14,3 +14,27 @@ class Node{
             this->before = nullptr;
         }
 };
+
+class DLL{
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+
+    public:
+        DLL(){
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->length = 0;
+        }
+
+        ~DLL(){
+            while (this->head != nullptr){
+                Node* temp_node = this->head;
+                this->head = this->head->next;
+                delete temp_node;
+            }
+            this->tail = nullptr;
+            this->length = 0;
+        }
+};
