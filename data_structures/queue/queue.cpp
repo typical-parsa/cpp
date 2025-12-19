9f634f2a02c8429e2b6662a81afc48e6bbced6b5
typicalparsa <typicalparsa@gmail.com>
2025-12-19 22:25:24 +0330

    cpp data_structure queue Node class

---
+++ data_structures/queue/queue.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node* next;
+
+        Node(int value){
+            this->value = value;
+            this->next = nullptr;
+        }
+};
+
