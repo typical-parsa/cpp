a0bb66d19292e4400a5246b30e2e1f4ab700cc9b
typicalparsa <typicalparsa@gmail.com>
2025-12-20 17:17:41 +0330

    cpp data_structure stack with array print stack

---
+++ data_structures/stack/with_array/stack.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main(){
+
+}
+
+void print_stack(int arr[], int size){
+    for (int i = 0 ; i < size ; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}  
