fbdf670eb60c52487e11ffca93cca3d4d2c7bff9
typicalparsa <typicalparsa@gmail.com>
2025-12-21 15:27:37 +0330

    cpp algorithms sort selection_sort show_array

---
+++ algorithm/sort/selection_sort/selection_sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void show_array(int arr[], int size);
+
+int main(){
+
+
+}
+
+
+void show_array(int arr[], int size){
+    for (int i = 0 ; i < size ; i++){
+        cout << arr[i] << endl;
+    }
+    cout << endl;
+}
