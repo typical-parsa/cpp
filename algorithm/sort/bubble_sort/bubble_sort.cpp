d3a211c18dc68baa6990448865f87190b92a8d0a
typicalparsa <typicalparsa@gmail.com>
2025-12-21 15:16:07 +0330

    cpp algorithms sort bubble_sort

--- algorithm/sort/bubble_sort/bubble_sort.cpp
+++ algorithm/sort/bubble_sort/bubble_sort.cpp
@@ -4,9 +4,25 @@ using namespace std;
 
 void show_array(int arr[], int size);
 void swap_element(int arr[], int first_index, int second_index);
+void bubble_sort(int arr[], int size);
 
 int main(){
 
+}
+
+void bubble_sort(int arr[], int size){
+    for (int i = 0 ; i < size - 1; i++){
+        bool swapped = false;
+        for (int j = 0 ; j < size - i ; j++){
+            if (arr[j] > arr[j + 1]){
+                swap_element(arr, j, j + 1);
+                swapped = true;
+            }
+        }
+        if (!swapped){
+            break;
+        }
+    }
 }
 
 void swap_element(int arr[], int first_index, int second_index){
