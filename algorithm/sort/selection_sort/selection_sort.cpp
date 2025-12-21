3a87bc2acfa231325a39c11596e612f03977a0f9
typicalparsa <typicalparsa@gmail.com>
2025-12-21 15:29:50 +0330

    cpp algorithms sort selection_sort swap_element

--- algorithm/sort/selection_sort/selection_sort.cpp
+++ algorithm/sort/selection_sort/selection_sort.cpp
@@ -3,12 +3,18 @@
 using namespace std;
 
 void show_array(int arr[], int size);
+void swap_element(int arr[], int first_index, int second_index);
 
 int main(){
 
 
 }
 
+void swap_element(int arr[], int first_index, int second_index){
+    int temp_value = arr[first_index];
+    arr[first_index] = arr[second_index];
+    arr[second_index] = temp_value;
+}
 
 void show_array(int arr[], int size){
     for (int i = 0 ; i < size ; i++){
