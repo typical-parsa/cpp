abedd9f30202135f28eb23b62f50979c34339339
typicalparsa <typicalparsa@gmail.com>
2025-12-22 23:23:13 +0330

    cpp algorithms sort insertion_sort

--- algorithm/sort/insertion_sort/insertion_sort.cpp
+++ algorithm/sort/insertion_sort/insertion_sort.cpp
@@ -4,10 +4,26 @@ using namespace std;
 
 void show_array(int arr[], int size);
 void swap_element(int arr[], int first_index, int second_index);
+void insertion_sort(int arr[], int size);
 
 int main(){
 
 }
+
+void insertion_sort(int arr[], int size){
+    for (int i = 1; i < size; i++){
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
 
 void swap_element(int arr[], int first_index, int second_index){
     int temp_value = arr[first_index];
