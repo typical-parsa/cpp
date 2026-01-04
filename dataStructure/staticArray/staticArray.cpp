5357a2d954814002161d0c6b8c918ee7d81b5e1e
typicalparsa <typicalparsa@gmail.com>
2026-01-04 21:49:09 +0330

    cpp dataStructure staticArray append

--- dataStructure/staticArray/staticArray.cpp
+++ dataStructure/staticArray/staticArray.cpp
@@ -3,11 +3,16 @@
 using namespace std;
 
 void printArray(int arr[], int size);
+bool append(int arr[], int &size, int capacity, int value);
 
 int main(){
     int arr[100];
     int size = 0;
     int capacity = 100;
+    append(arr, size, capacity, 1);
+    append(arr, size, capacity, 2);
+    printArray(arr,size);
+    
 
 }
 
@@ -16,4 +21,13 @@ void printArray(int arr[], int size){
         cout << arr[i] << " ";
     }
     cout << endl;
+}
+
+bool append(int arr[], int &size, int capacity, int value){
+    if (size >= capacity){
+        return false;
+    }
+    arr[size] = value;
+    size++;
+    return true;
 }
