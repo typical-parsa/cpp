c1696093bc6ebf5d68c0d7ff4baf4bd08e8b664b
typicalparsa <typicalparsa@gmail.com>
2026-01-06 22:20:28 +0330

    cpp dataStructure singularLinkedList Node class

---
+++ dataStructure/linkedList/singularLinkedList/singularLinkedList.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node* next;
+
+        Node(int value){
+            this->value = value;
+            this->next = nullptr;
+        }
+};
