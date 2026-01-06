e7503acf349a3aebc76aa5dc4c139e1e4cf1ca88
typicalparsa <typicalparsa@gmail.com>
2026-01-06 22:22:37 +0330

    cpp dataStructure singularLinkedList singularLinkedList class

--- dataStructure/linkedList/singularLinkedList/singularLinkedList.cpp
+++ dataStructure/linkedList/singularLinkedList/singularLinkedList.cpp
@@ -12,3 +12,27 @@ class Node{
             this->next = nullptr;
         }
 };
+
+class singularLinkedList{
+    private:
+        Node* head;
+        Node* tail;
+        int lenght;
+
+    public:
+        singularLinkedList(){
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->lenght;
+        }
+
+        ~singularLinkedList(){
+            while (this->head != nullptr){
+                Node* tempNode = this->head;
+                this->head = this->head->next;
+                delete tempNode;
+            }
+            this->tail = nullptr;
+            this->lenght = 0;
+        }
+};
