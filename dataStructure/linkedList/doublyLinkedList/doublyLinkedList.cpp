86a6caa0f294d2a3215cdce2df20809e8a4fc31a
typicalparsa <typicalparsa@gmail.com>
2026-01-09 07:52:22 +0330

    cpp dataStructure doublyLinkedList Node class

---
+++ dataStructure/linkedList/doublyLinkedList/doublyLinkedList.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node* next;
+        Node* prev;
+
+        Node(int value){
+            this->value = value;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
