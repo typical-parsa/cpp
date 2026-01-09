1167972269c42f439762dac34ebd33cc134cc91b
typicalparsa <typicalparsa@gmail.com>
2026-01-09 07:57:36 +0330

    cpp dataStructure doublyLinkedList appendNode

--- dataStructure/linkedList/doublyLinkedList/doublyLinkedList.cpp
+++ dataStructure/linkedList/doublyLinkedList/doublyLinkedList.cpp
@@ -49,4 +49,20 @@ class doublyLinkedList{
                 cout << endl;
             }
         }
+
+        bool appendNode(int value){
+            Node* newNode = new Node(value);
+            if (this->length == 0){
+                this->head = newNode;
+                this->tail = newNode;
+            }else{
+                this->tail->next = newNode;
+                newNode->prev = this->tail;
+                this->tail = newNode;
+            }
+            this->length++;
+            return true;
+        }
+
+
 };
