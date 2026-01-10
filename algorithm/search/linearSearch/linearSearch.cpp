a34347e0ef1a27008dd0de65bd88de6c28c4b88b
typicalparsa <typicalparsa@gmail.com>
2026-01-10 22:41:13 +0330

    cpp algorithms linearSearch linearSearch

---
+++ algorithm/search/linearSearch/linearSearch.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int linearSearch(int arr[], int size, int target);
+
+
+int linearSearch(int arr[], int size, int target){
+    for (int i = 0 ; i < size ; i++){
+        if (arr[i] == target){
+            return i;
+        }
+    }
+    return INT_MIN;
+}
