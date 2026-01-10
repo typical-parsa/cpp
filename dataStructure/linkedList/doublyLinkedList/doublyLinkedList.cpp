44cb296e77c00d887545cba695bfda7cbb566c9e
typicalparsa <typicalparsa@gmail.com>
2026-01-10 20:25:26 +0330

    cpp dataStructure doublyLinkedList prependNode

--- dataStructure/linkedList/doublyLinkedList/doublyLinkedList.cpp
+++ dataStructure/linkedList/doublyLinkedList/doublyLinkedList.cpp
@@ -81,5 +81,19 @@ class doublyLinkedList{
             return true;
         }
 
+        bool prependNode(int value){
+            Node* newNode = new Node(value);
+            if (this->length == 0){
+                this->head = newNode;
+                this->tail = newNode;
+            }else{
+                this->head->prev = newNode;
+                newNode->next = this->head;
+                this->head = newNode;
+            }
+            this->length++;
+            return true;
+        }
+
 
 };
