ff752144c0cffaaa0782257b64dc9e11de211f8c
typicalparsa <typicalparsa@gmail.com>
2026-01-12 23:24:51 +0330

    cpp dataStructure stack Stack class

--- dataStructure/stack/stack.cpp
+++ dataStructure/stack/stack.cpp
@@ -10,6 +10,28 @@ class Node{
 
         Node(int value){
             this->value = value;
-            this->next = nullptr
+            this->next = nullptr;
         }
+};
+
+class Stack{
+    private:
+        Node* top;
+        int height;
+
+    public:
+        Stack(){
+            this->top = nullptr;
+            this->height = 0;
+        }
+
+        ~Stack(){
+            while (this->top != nullptr){
+                Node* tempNode = this->top;
+                this->top = this->top->next;
+                delete tempNode;
+            }
+            this->height = 0;
+        }
+
 };
