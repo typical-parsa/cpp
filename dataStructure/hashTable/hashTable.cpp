35950f5883bf1b6caddc610f77c002e1f55840ee
typicalparsa <typicalparsa@gmail.com>
2026-01-13 22:00:39 +0330

    cpp dataStructure hashTable hashTable class

--- dataStructure/hashTable/hashTable.cpp
+++ dataStructure/hashTable/hashTable.cpp
@@ -15,3 +15,30 @@ class Node{
             this->next = nullptr;
         }
 };
+
+class hashTable{
+    private:
+        int size;
+        Node** dataMap;
+    
+    public:
+        hashTable(int tableSize){
+            this->size = tableSize;
+            this->dataMap = new Node*[size];
+            for (int i = 0 ; i < size ; i++){
+                this->dataMap[i] = nullptr;
+            }
+        }
+
+        ~hashTable(){
+            for (int i = 0 ; i < this->size ; i++){
+                Node* currentNode = this->dataMap[i];
+                while (currentNode != nullptr){
+                    Node* tempNode = currentNode;
+                    currentNode = currentNode->next;
+                    delete tempNode;
+                }
+            }
+            delete[] this->dataMap;
+        }
+};
