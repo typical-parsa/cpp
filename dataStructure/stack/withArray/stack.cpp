b75249124adc0acaf220f3c08ea69d39f831d85f
typicalparsa <typicalparsa@gmail.com>
2026-02-08 15:03:14 +0330

    cpp dataStructure stack printStack

---
+++ dataStructure/stack/withArray/stack.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+void printStack(int arr[], int size);
+
+int main(){
+
+}
+
+void printStack(int arr[], int size){
+    for (int i = 0 ; i < size ; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
