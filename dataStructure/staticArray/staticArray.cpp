a273da8ecfea311a2fce6f69f7cd71f48fdf81b4
typicalparsa <typicalparsa@gmail.com>
2026-06-09 13:24:52 +0330

    cpp dataStructure staticArray prependElement

--- dataStructure/staticArray/staticArray.cpp
+++ dataStructure/staticArray/staticArray.cpp
@@ -22,3 +22,15 @@ bool deleteLastElement(int arr[], int &size){
     size--;
     return true;
 }
+
+bool prependElement(int arr[], int &size, int capacity, int value){
+    if (size >= capacity){
+        return false;
+    }
+    for (int i = size ; i > 0 ; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[0] = value;
+    size++;
+    return true;
+}
