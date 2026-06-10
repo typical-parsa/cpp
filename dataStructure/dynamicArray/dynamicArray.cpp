dabe79715f5ba262d5efe7afbfb493fbff43b331
typicalparsa <typicalparsa@gmail.com>
2026-06-10 15:54:43 +0330

    cpp dataStructure dynamicArray resize

---
+++ dataStructure/dynamicArray/dynamicArray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class dynamicArray{
+    private:
+        int length;
+        int capacity;
+        int* arrayUnderHood;
+
+        void resize(){
+            int newCapacity;
+            if (this->capacity == 0){
+                newCapacity = 1;
+            }else{
+                newCapacity = this->capacity * 2;
+            }
+            int* newArrayUnderHood = new int[newCapacity];
+            for (int i = 0 ; i < this->size ; i++){
+                newArrayUnderHood[i] = this->arrayUnderHood[i];
+            }
+            delete[] this->arrayUnderHood;
+            this->capacity = newCapacity;
+            this->arrayUnderHood = newArrayUnderHood;
+        }
+}
