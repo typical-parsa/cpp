4ba8518d4e4272d35efefac9e061d834351f38c7
typicalparsa <typicalparsa@gmail.com>
2026-06-11 09:42:26 +0330

    cpp dataStructure dynamicArray getCapacity

--- dataStructure/dynamicArray/dynamicArray.cpp
+++ dataStructure/dynamicArray/dynamicArray.cpp
@@ -66,7 +66,11 @@ class dynamicArray{
             return this->arrayUnderHood[index];
         }
 
-        int size(){
+        int getSize(){
             return this->length;
+        }
+
+        int getCapacity(){
+            return this->capacity;
         }
 };
