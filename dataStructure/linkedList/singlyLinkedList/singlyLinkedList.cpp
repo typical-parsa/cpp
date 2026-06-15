ae3041fd09f21e4a3d54af10af92510dab435ce2
typicalparsa <typicalparsa@gmail.com>
2026-06-15 10:01:25 +0330

    cpp dataStructure linkedList singlyLinkedList strcture classes

---
+++ dataStructure/linkedList/singlyLinkedList/singlyLinkedList.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node* next;
+
+        Node(int value){
+            this->value = value;
+            this->next = nullptr;
+        }
+};
+
+class SLL{
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+
+
+    public:
+        SLL(){
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->length = 0;
+        }
+
+        ~SLL(){
+            while (this->head != nullptr){
+                Node* tempNode = this->head;
+                this->head = this->head->next;
+                delete tempNode;
+            }
+            this->tail = nullptr;
+            this->length = 0;
+        }
+};
